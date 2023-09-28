5af3e2663a3762bc1de0809dda02f6cef67039b1
Veronica Perez <veperez@live.com>
2023-09-28 17:00:46 -0500

    password generated

--- develop/script.js
+++ develop/script.js
@@ -2,46 +2,33 @@
 var generateBtn = document.querySelector("#generate");
 
 // <!--ONLY TOUCH HERE-->
-function generatePassword(){
-
-  var allLettersNumbersSymbols= "ABCDEFGHIJKLMNOPQRSTUVWXYZbcdefghijklmnopqrstuvwxyz123456789@#$%^&*-+=";
-  // var lowerCaser= "abcdefghijklmnopqrstuvwxyz";
-  // var numbers= "0123456789";
-  // var symbols= "!@#$%^&*-+=";
-  var passwordLength= 10;
-  var password= "";
+function generatePassword() {
+  var allLettersNumbersSymbols =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZbcdefghijklmnopqrstuvwxyz123456789@#$%^&*-+=";
+  var passwordLength = 10;
+  var password = "";
 
   for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * allLettersNumbersSymbols.length); password += allLettersNumbersSymbols.substring(randomNumber, randomNumber +1);
-    }
-
-      document.querySelector("#generate") .value=password;
-      // passwordText.value = password;
-      // generateBtn.addEventListener("click", writePassword);
-
-    }
-  
-  function writePassword() {
-    // var generateBtn = document.querySelector("#generate");
-    var passwordText = document.querySelector("#password");
-    passwordText.select();
-    passwordText.setSelectionRange(0, 999);
-    document.execCommand("copy");
-  
+    var randomNumber = Math.floor(
+      Math.random() * allLettersNumbersSymbols.length
+    );
+    password += allLettersNumbersSymbols.substring(
+      randomNumber,
+      randomNumber + 1
+    );
   }
+  return password
 
-  var generatePassword
-  
+}
 
-  // this is called a prompt
-  
-  // var name = prompt("What is your name");
+// var generatePassword
 
-  // and this is called a confirm
-  // var areThePackersAwesome =confirm("Are the Packers awesome?")
+// this is called a prompt
 
+// var name = prompt("What is your name");
 
-
+// and this is called a confirm
+// var areThePackersAwesome =confirm("Are the Packers awesome?")
 
 // DO NOT TOUCHWrite password to the #password input
 function writePassword() {
@@ -49,7 +36,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // DO NOT TOUCH Add event listener to generate button
